51248f800d85bbdcbc00127b8d88d21022ad9616
bertie2 <georgemansbridge@msn.com>
2023-07-16 23:31:54 +0100

    run formatter

--- cpp/include/cubes.hpp
+++ cpp/include/cubes.hpp
@@ -2,6 +2,7 @@
 #ifndef OPENCUBES_CUBES_HPP
 #define OPENCUBES_CUBES_HPP
 #include <stdint.h>
+
 #include "hashes.hpp"
 
 Hashy gen(uint n, int threads = 1, bool use_cache = false, bool write_cache = false);
--- cpp/src/program.cpp
+++ cpp/src/program.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 
-#include "cubes.hpp"
 #include "cmdparser.hpp"
+#include "cubes.hpp"
 
-void configure_arguments(cli::Parser& parser){
+void configure_arguments(cli::Parser& parser) {
     parser.set_required<int>("n", "cube_size", "the size of polycube to generate up to");
     parser.set_required<int>("t", "threads", "the number of threads to use while generating");
     parser.set_optional<bool>("c", "use_cache", false, "whether to load cache files");
     parser.set_optional<bool>("w", "write_cache", false, "wheather to save cache files");
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
     cli::Parser parser(argc, argv);
     configure_arguments(parser);
     parser.run_and_exit_if_error();

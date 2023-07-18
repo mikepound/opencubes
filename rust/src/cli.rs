45eeee5ed86098c843fd8858512dc5d62038577f
datdenkikniet <jcdra1@gmail.com>
2023-07-18 17:33:40 +0200

    Some naming and moving

--- rust/src/cli.rs
+++ rust/src/cli.rs
@@ -175,7 +175,7 @@ pub fn validate(opts: &ValidateArgs) -> std::io::Result<()> {
         }
     };
 
-    let mut file = PolyCubeFile::new(path)?;
+    let mut file = PolyCubeFile::new_file(path)?;
     file.should_canonicalize = false;
     let canonical = file.canonical();
     let len = file.len();
@@ -268,7 +268,7 @@ pub fn validate(opts: &ValidateArgs) -> std::io::Result<()> {
 fn load_cache_file(n: usize) -> Option<PolyCubeFile> {
     let name = format!("cubes_{n}.pcube");
 
-    match PolyCubeFile::new(&name) {
+    match PolyCubeFile::new_file(&name) {
         Ok(file) => Some(file),
         Err(e) => {
             if e.kind() == ErrorKind::InvalidData || e.kind() == ErrorKind::Other {
@@ -430,7 +430,7 @@ pub fn convert(opts: &ConvertArgs) {
     println!("Converting file {}", opts.path);
     println!("Final output path: {output_path}");
 
-    let mut input_file = match PolyCubeFile::new(&opts.path) {
+    let mut input_file = match PolyCubeFile::new_file(&opts.path) {
         Ok(f) => f,
         Err(e) => {
             println!("Failed to open input file. Error: {e}");
@@ -511,7 +511,7 @@ pub fn convert(opts: &ConvertArgs) {
 }
 
 fn info(path: &str) {
-    let file = match PolyCubeFile::new(path) {
+    let file = match PolyCubeFile::new_file(path) {
         Ok(f) => f,
         Err(e) => {
             println!("Failed to open file. {e}");

96a3c01701a6a46fc55b6ddf583c51a94661fefb
datdenkikniet <jcdra1@gmail.com>
2023-07-18 20:40:21 +0200

    Don't use cache file N, but N - 1

--- rust/src/cli.rs
+++ rust/src/cli.rs
@@ -300,7 +300,7 @@ where
         let mut calculate_from = 2;
 
         if use_cache {
-            for n in (calculate_from..=n).rev() {
+            for n in (calculate_from..n).rev() {
                 let name = format!("cubes_{n}.pcube");
                 let cache = if let Some(file) = load_cache_file(n) {
                     file
@@ -379,6 +379,8 @@ where
                         name,
                     )
                     .unwrap();
+                } else {
+                    println!("Cache file already exists for N = {i}. Not overwriting.");
                 }
             }
 
